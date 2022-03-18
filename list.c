6528d13ddc6960e328317ffa3c5de9efe4c75799
IGNACIOTOMASTOM <>
2022-03-18 14:56:24 +0000

    new try

--- list.c
+++ list.c
@@ -44,9 +44,11 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-    if (list->current->next != NULL)
+    if (list->current->next != NULL){
       list->current = list->current->next;
-    return list->current->data;
+      return list->current->data;
+      }
+    return NULL;
 }
 
 void * lastList(List * list) {
