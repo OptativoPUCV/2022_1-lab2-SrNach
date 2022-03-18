765bceabb4c727ed235f804efecbf36e8695b4c4
IGNACIOTOMASTOM <>
2022-03-18 14:47:09 +0000

    new try

--- list.c
+++ list.c
@@ -37,7 +37,8 @@ List * createList() {
 }
 
 void * firstList(List * list) {
-    list->current = list->head;
+    if (list->head != NULL)
+      list->current = list->head;
     return list->head;
 }
 
